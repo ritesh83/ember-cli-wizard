61b9c1d90455b1b0b0e0d4fa6b692242d35dc38d
Ritesh Keswani <ritesh.keswani@panasonic.aero>
2016-12-14 21:50:09 -0800

    Updated the dummy app

--- tests/dummy/app/controllers/form-example.js
+++ tests/dummy/app/controllers/form-example.js
@@ -3,27 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
     wizardData: [
-        {'step_id': '1', 'header_label': '1. First Step', 'hasAction': true},
-        {'step_id': '2', 'header_label': '2. Second Step'},
-        {'step_id': '3', 'header_label': '3. Third Step'}
+        {'step_id': '1', 'header_label': '1. Account'},
+        {'step_id': '2', 'header_label': '2. Profile'}
     ],
-
-    wizardShowNextStep: true,
 
     actions: {
 
-        cancel() {
+        cancelAction() {
             //window.history.back();
         },
 
         submitAction() {
             console.log('submitAction');
-        },
-
-        wizardStepChanged(wizardStep) {
-            if (wizardStep['step_id'] === '1') {
-                this.set('wizardShowNextStep', true);
-            }
         }
 
     }
